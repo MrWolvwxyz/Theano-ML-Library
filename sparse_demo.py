6e29b2d923656b36581a929cb2f970713daf6855
Sam <sabean@umich.edu>
2014-01-01 22:33:24 -0500

    Create sparse_demo.py

---
+++ sparse_demo.py
@@ -0,0 +1,22 @@
+import os
+import re
+import theano
+import string
+import time
+
+import theano.tensor as T
+from theano.sandbox.linalg.ops import MatrixInverse as matrix_inverse
+
+import numpy as np
+import numpy.random as random
+
+from sparse_lib import *
+
+sample_input = random.rand( 5, 4 )
+
+model = sparsity_model( sample_input )
+model.feature_sign_search()
+
+
+
+
